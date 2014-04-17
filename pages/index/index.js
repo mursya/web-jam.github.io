06041ccb141303384c0acf5fd630419a3fbe2fb4
Сергей Сергеев <gurugray@yandex-team.ru>
2014-04-17 20:45:10 +0400

    BEM: page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,1 @@
-var map = L.mapbox.map(
-	'map',
-	'gurugray.ho2fj0do',
-	{
-		zoomControl: false,
-		shareControl: false
-	})
-    .setView([44.955, 34.083], 16);
-
-map.touchZoom.disable();
-map.doubleClickZoom.disable();
-map.scrollWheelZoom.disable();
-
-// disable tap handler, if present.
-if (map.tap) map.tap.disable();
+/* borschik:include:../../blocks/page/page.js */
